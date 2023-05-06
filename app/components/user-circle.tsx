42ba2dfc5b2d874d697a4703b298949395db34f6
jeremy collyer <collyerdesign@gmail.com>
2023-05-06 13:12:22 -0400

    update to push

--- app/components/user-circle.tsx
+++ app/components/user-circle.tsx
@@ -22,7 +22,6 @@ export function UserCircle({ profile, onClick, className }: props) {
         <h2>
           {profile.firstName.charAt(0).toUpperCase()}
           {profile.lastName.charAt(0).toUpperCase()}
-          
         </h2>
       )}
     </div>
